70c5680d144938c19718b93cc9d132de1d8ee1db
Daylan-lewandowski <74326816+Daylan-lewandowski@users.noreply.github.com>
2021-06-10 19:25:04 -0500

    fixed database

---
+++ public/js/idb.js
@@ -0,0 +1,82 @@
+
+let db;
+
+const request = indexedDB.open('budget_tracker',1);
+
+request.onupgradeneeded = function(event) {
+
+const db = event.target.result;
+    
+    db.createObjectStore('budget_tracker', {autoIncrement: true});
+};
+  
+  request.onsuccess = function(event) {
+   
+    db = event.target.result;
+  
+    
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+  };
+
+  request.onerror = function(event) {
+
+    console.log(event.target.errorCode);
+  };
+
+ 
+  
+  function saveRecord(record) {
+
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
+  
+
+    const budgetObjectStore = transaction.objectStore('budget_tracker');
+  
+    budgetObjectStore.add(record);
+};
+
+function uploadBudget() {
+
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
+  
+
+    const budgetObjectStore = transaction.objectStore('budget_tracker');
+  
+
+    const getAll = budgetObjectStore.getAll();
+  
+
+ getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+    fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(serverResponse => {
+        if (serverResponse.message) {
+            throw new Error(serverResponse);
+        }
+        // open one more transaction
+        const transaction = db.transaction(['budget_tracker'], 'readwrite');
+        // access the  object store
+        const pizzaObjectStore = transaction.objectStore('budget_tracker');
+        // clear all items in your store
+        pizzaObjectStore.clear();
+
+        alert('Budget additions are now submitted');
+        })
+        .catch(err => {
+        console.log(err);
+        });
+    }
+};
+};
+
+window.addEventListener('online', uploadBudget)
